236983b39ed189edf4a054f5dbca58173bd8789a
Hyunbin Kim <tylerhkmontana@gmail.com>
2020-04-26 04:00:31 -0400

    Fix typo and fix flash msg typo

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
         if (req.isAuthenticated()) {
             return next()
         } 
-        req.flash("error_message", "Please log in to view this resource")
+        req.flash("error_msg", "Please log in to view this resource")
         res.redirect("/users/login")
     },
     forwardAuthenticated: function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -34,7 +34,7 @@ router.post("/register", (req, res) => {
 
     // Check pass length
     if (password.length < 6) {
-        errors.push({ msg: "Password should be at 6 characters" })
+        errors.push({ msg: "Password should be at least 6 characters" })
     }
 
     if (errors.length > 0) {
